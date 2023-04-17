5caca03d342b2a79a847496d8b6777deec06c614
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-17 17:17:27 -0400

    script that counts characters in king james bible

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,23 @@
 import * as fs from "fs";
-fs.readFileSync("foo.txt", "utf8");
+const fileContents = fs.readFileSync("kjb.txt", "utf8") as string;
+
+const charCount = {} as { [key: string]: number };
+var maxCount = 0;
+var maxChar = "";
+for (const char of fileContents) {
+  if (!charCount[char]) {
+    // check if new char
+    charCount[char] = 1;
+  } else {
+    // if not new, increment count of that char by 1
+    charCount[char]++;
+  }
+  if (charCount[char] > maxCount) {
+    // track maximum count and char
+    maxCount = charCount[char];
+    maxChar = char;
+  }
+}
+
+console.log("' " + maxChar + " '" + "count: " + maxCount);
+console.log("done");
