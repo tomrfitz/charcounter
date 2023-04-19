fe78ccb4269660259e3c42c576ee76a3fc5aa150
Thomas FitzGerald <tomrfitz@gmail.com>
2023-04-19 14:17:30 -0400

    remove unnecessary files

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-function twoSum(nums: number[], target: number): number[] {
-  const map = new Map();
-  for (let i = 0; i < nums.length; i++) {
-    var complement = target - nums[i];
-    if (map.has(complement)) return [i, map.get(nums[i])];
-    map.set(nums[i], i);
-  }
-  return [];
-}
